ed674dbfccd652fa4f3ecc474ca8f1d42debe66a
Lucas Cardoso <lucasgavirachicc@gmail.com>
2021-06-28 22:39:09 +0000

    Adding roteiro test

--- src/test/java/com/bcopstein/RoteiroTest.java
+++ src/test/java/com/bcopstein/RoteiroTest.java
@@ -1,6 +1,8 @@
 package com.bcopstein;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -8,6 +10,7 @@ import java.util.List;
 
 import com.bcopstein.entidades.Bairro;
 import com.bcopstein.entidades.Roteiro;
+import com.bcopstein.entidades.geometria.Area;
 import com.bcopstein.entidades.geometria.Ponto;
 import com.bcopstein.entidades.geometria.Reta;
 
@@ -20,29 +23,57 @@ public class RoteiroTest {
     @BeforeEach
     public void setup(){
         bairros = new ArrayList<>();
-        bairros.add(Bairro.novoBairroRetangular("Bom Fim", new Ponto(10,40), 20, 10, 10.0));
-        bairros.add(Bairro.novoBairroRetangular("Independencia", new Ponto(30,40), 20, 10, 20.0));
-        bairros.add(Bairro.novoBairroRetangular("Moinhos de Vento", new Ponto(20,30), 20, 10, 30.0));
-        bairros.add(Bairro.novoBairroRetangular("Auxiliadora", new Ponto(40,30), 20, 10, 20.0));
-        bairros.add(Bairro.novoBairroRetangular("Boa Vista", new Ponto(40,20), 20, 10, 20.0));
+
+        bairros.add(new Bairro("teste", new Area(new Ponto(1, 4), new Ponto(4, 1)), 20));
+        bairros.add(new Bairro("teste2", new Area(new Ponto(4, 4), new Ponto(7, 1)), 30));
+        bairros.add(new Bairro("teste3", new Area(new Ponto(4, 7), new Ponto(7, 4)), 10));
+        // bairros.add(Bairro.novoBairroRetangular("Independencia", new Ponto(30,40), 20, 10, 20.0));
+        // bairros.add(Bairro.novoBairroRetangular("Moinhos de Vento", new Ponto(20,30), 20, 10, 30.0));
+        // bairros.add(Bairro.novoBairroRetangular("Auxiliadora", new Ponto(40,30), 20, 10, 20.0));
+        // bairros.add(Bairro.novoBairroRetangular("Boa Vista", new Ponto(40,20), 20, 10, 20.0));
+    }
+
+    @Test
+    public void testaConstrutor(){
+        Roteiro roteiro = new Roteiro(bairros.get(0), bairros.get(1), bairros);   
+        assertTrue(bairros.get(0).equals(roteiro.getBairroOrigem()));
+        assertTrue(bairros.get(1).equals(roteiro.getBairroDestino()));
     }
 
     @Test
     public void testaRota(){
-        Roteiro roteiro = new Roteiro(bairros.get(1), bairros.get(4), bairros);
-        Reta rotaEsp = new Reta(new Ponto(40,35),new Ponto(50,15));
+        Roteiro roteiro = new Roteiro(bairros.get(0), bairros.get(1), bairros);
+        Reta rotaEsp = new Reta(new Ponto(2,2),new Ponto(5,2));
         assertEquals(rotaEsp,roteiro.getRota());
     }
 
     @Test
     public void testaBairrosPercorridos(){
-        Roteiro roteiro = new Roteiro(bairros.get(1), bairros.get(4), bairros);
+        Roteiro roteiro = new Roteiro(bairros.get(0), bairros.get(1), bairros);
         Collection<Bairro> esperado = new ArrayList<>();
+        esperado.add(bairros.get(0));
         esperado.add(bairros.get(1));
-        esperado.add(bairros.get(2));
-        esperado.add(bairros.get(3));
-        esperado.add(bairros.get(4));
         Collection<Bairro> observado = roteiro.bairrosPercoridos();
         assertEquals(esperado, observado);
+    }
+
+    @Test
+    public void testaBairrosPercorridos2(){
+        Roteiro roteiro = new Roteiro(bairros.get(0), bairros.get(2), bairros);
+        Collection<Bairro> esperado = new ArrayList<>();
+        esperado.add(bairros.get(0));
+        esperado.add(bairros.get(1));
+        esperado.add(bairros.get(2));
+        Collection<Bairro> observado = roteiro.bairrosPercoridos();
+        assertEquals(esperado, observado);
+    }
+
+    @Test
+    public void testaEquals(){
+        Roteiro roteiro = new Roteiro(bairros.get(0), bairros.get(1), bairros);
+        Roteiro roteiroIgual = new Roteiro(bairros.get(0), bairros.get(1), bairros);
+        Roteiro roteiroDiferente = new Roteiro(bairros.get(0), bairros.get(2), bairros);
+        assertTrue(roteiro.equals(roteiroIgual));
+        assertFalse(roteiro.equals(roteiroDiferente));
     }
 }
